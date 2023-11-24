82e4eca273933f6b8a99f741f24b83a58122d01a
Andreas Buis <abuis@splunk.com>
2023-11-24 11:56:33 +0000

    ss

--- prePOC URL check.py
+++ prePOC URL check.py
@@ -12,14 +12,14 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'filter_1' block
-    filter_1(container=container)
+    # call 'filter_url_artifact' block
+    filter_url_artifact(container=container)
 
     return
 
 @phantom.playbook_block()
-def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("filter_1() called")
+def filter_url_artifact(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("filter_url_artifact() called")
 
     # collect filtered artifact ids and results for 'if' condition 1
     matched_artifacts_1, matched_results_1 = phantom.condition(
@@ -27,12 +27,172 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
         conditions=[
             ["artifact:*.name", "==", "URL Artifact"]
         ],
-        name="filter_1:condition_1",
+        name="filter_url_artifact:condition_1",
         delimiter=None)
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        pass
+        url_reputation_vt(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+
+@phantom.playbook_block()
+def url_reputation_vt(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("url_reputation_vt() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    filtered_artifact_0_data_filter_url_artifact = phantom.collect2(container=container, datapath=["filtered-data:filter_url_artifact:condition_1:artifact:*.cef.requestURL","filtered-data:filter_url_artifact:condition_1:artifact:*.id"])
+
+    parameters = []
+
+    # build parameters list for 'url_reputation_vt' call
+    for filtered_artifact_0_item_filter_url_artifact in filtered_artifact_0_data_filter_url_artifact:
+        if filtered_artifact_0_item_filter_url_artifact[0] is not None:
+            parameters.append({
+                "url": filtered_artifact_0_item_filter_url_artifact[0],
+                "context": {'artifact_id': filtered_artifact_0_item_filter_url_artifact[1]},
+            })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("url reputation", parameters=parameters, name="url_reputation_vt", assets=["virustotal"], callback=filter_vt_result)
+
+    return
+
+
+@phantom.playbook_block()
+def filter_vt_result(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("filter_vt_result() called")
+
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["url_reputation_vt:action_result.summary.malicious", ">", 0]
+        ],
+        name="filter_vt_result:condition_1",
+        delimiter=None)
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        liste_malicious_greater_0(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    # collect filtered artifact ids and results for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        conditions=[
+            ["url_reputation_vt:action_result.summary.malicious", "==", 0]
+        ],
+        name="filter_vt_result:condition_2",
+        delimiter=None)
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        liste_malicious_gleich_0(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
+
+    return
+
+
+@phantom.playbook_block()
+def liste_malicious_greater_0(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("liste_malicious_greater_0() called")
+
+    template = """Das ist die Liste aller URL die einen malicious score größer 0 haben:\n%%\n{0}\n%%"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "filtered-data:filter_vt_result:condition_1:url_reputation_vt:action_result.parameter.url"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="liste_malicious_greater_0")
+
+    join_report(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def liste_malicious_gleich_0(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("liste_malicious_gleich_0() called")
+
+    template = """Das ist die Liste aller URL die einen malicious score größer 0 haben:\n%%\n{0}\n%%\n"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "filtered-data:filter_vt_result:condition_2:url_reputation_vt:action_result.parameter.url"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="liste_malicious_gleich_0")
+
+    join_report(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def join_report(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("join_report() called")
+
+    if phantom.completed(action_names=["url_reputation_vt"]):
+        # call connected block "report"
+        report(container=container, handle=handle)
+
+    return
+
+
+@phantom.playbook_block()
+def report(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("report() called")
+
+    template = """Das sind die Ergebnisse vom VT Check:\n\n{0}\n\n######\n{1}"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "liste_malicious_greater_0:formatted_data",
+        "liste_malicious_gleich_0:formatted_data"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="report")
 
     return
 
