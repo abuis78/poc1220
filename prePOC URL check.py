a57328840127031bbaf61d31eca4241dad067f1f
Andreas Buis <abuis@splunk.com>
2023-11-24 11:49:24 +0000

    ss

--- prePOC URL check.py
+++ prePOC URL check.py
@@ -12,13 +12,38 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-
+    # call 'filter_1' block
+    filter_1(container=container)
 
     return
 
 @phantom.playbook_block()
+def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("filter_1() called")
+
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.name", "==", "URL Artifact"]
+        ],
+        name="filter_1:condition_1",
+        delimiter=None)
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        pass
+
+    return
+
+
+@phantom.playbook_block()
 def on_finish(container, summary):
     phantom.debug("on_finish() called")
+
+    output = {
+        "report": [],
+    }
 
     ################################################################################
     ## Custom Code Start
@@ -29,5 +54,7 @@ def on_finish(container, summary):
     ################################################################################
     ## Custom Code End
     ################################################################################
+
+    phantom.save_playbook_output_data(output=output)
 
     return
