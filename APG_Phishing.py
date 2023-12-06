8136f045c606f0c350bddd5b2b68b09b360ba3eb
Andreas <Schmoll>
2023-12-06 13:30:46 +0000

    comment

--- APG_Phishing.py
+++ APG_Phishing.py
@@ -12,9 +12,61 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-
+    # call 'filter_1' block
+    filter_1(container=container)
 
     return
+
+@phantom.playbook_block()
+def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("filter_1() called")
+
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.tags", "==", "reported_mail"]
+        ],
+        name="filter_1:condition_1",
+        delimiter=None)
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        extract_ioc_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+
+@phantom.playbook_block()
+def extract_ioc_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("extract_ioc_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    parameters = []
+
+    parameters.append({
+        "severity": "medium",
+        "parse_domains": True,
+        "run_automation": True,
+        "remap_cef_fields": "Do not apply CEF -> CIM remapping, only apply custom remap",
+        "custom_remap_json": "{}",
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("extract ioc", parameters=parameters, name="extract_ioc_1", assets=["parser"])
+
+    return
+
 
 @phantom.playbook_block()
 def on_finish(container, summary):
