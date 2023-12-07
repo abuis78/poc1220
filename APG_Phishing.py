63c8762c199a8df86df19307430442ed86decf00
Andreas <Schmoll>
2023-12-07 13:38:11 +0000

    a

--- APG_Phishing.py
+++ APG_Phishing.py
@@ -25,7 +25,7 @@ def select_reported_mail(action=None, success=None, container=None, results=None
     matched_artifacts_1, matched_results_1 = phantom.condition(
         container=container,
         conditions=[
-            ["reported_mail", "in", "artifact:*.tags"]
+            ["artifact:*.name", "==", "Vault Artifact"]
         ],
         name="select_reported_mail:condition_1",
         delimiter=None)
@@ -33,6 +33,19 @@ def select_reported_mail(action=None, success=None, container=None, results=None
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
         extract_ioc_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    # collect filtered artifact ids and results for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        conditions=[
+            [".eml", "in", "artifact:*.cef.fileName"]
+        ],
+        name="select_reported_mail:condition_2",
+        delimiter=None)
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        pass
 
     return
 
@@ -44,24 +57,24 @@ def extract_ioc_1(action=None, success=None, container=None, results=None, handl
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
     id_value = container.get("id", None)
-    filtered_artifact_0_data_select_reported_mail = phantom.collect2(container=container, datapath=["filtered-data:select_reported_mail:condition_1:artifact:*.cef.bodyHtml","filtered-data:select_reported_mail:condition_1:artifact:*.id","filtered-data:select_reported_mail:condition_1:artifact:*.external_id"])
+    filtered_artifact_0_data_select_reported_mail = phantom.collect2(container=container, datapath=["filtered-data:select_reported_mail:condition_1:artifact:*.cef.vaultId","filtered-data:select_reported_mail:condition_1:artifact:*.id","filtered-data:select_reported_mail:condition_1:artifact:*.external_id"])
 
     parameters = []
 
     # build parameters list for 'extract_ioc_1' call
     for filtered_artifact_0_item_select_reported_mail in filtered_artifact_0_data_select_reported_mail:
         parameters.append({
+            "text": "",
+            "label": "",
             "severity": "medium",
+            "vault_id": filtered_artifact_0_item_select_reported_mail[0],
+            "file_type": "email",
+            "container_id": id_value,
+            "artifact_tags": "reported_mail",
             "parse_domains": False,
             "run_automation": False,
             "remap_cef_fields": "Do not apply CEF -> CIM remapping, only apply custom remap",
             "custom_remap_json": "{}",
-            "vault_id": "",
-            "file_type": "html",
-            "label": "",
-            "artifact_tags": "reported_mail",
-            "container_id": id_value,
-            "text": filtered_artifact_0_item_select_reported_mail[0],
             "context": {'artifact_id': filtered_artifact_0_item_select_reported_mail[1], 'artifact_external_id': filtered_artifact_0_item_select_reported_mail[2]},
         })
 
