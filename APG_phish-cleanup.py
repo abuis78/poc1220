2891ce387ea20cd66a43355578ba7e04d8b79b03
Andreas <Schmoll>
2023-12-11 11:57:36 +0000

    a

--- APG_phish-cleanup.py
+++ APG_phish-cleanup.py
@@ -178,7 +178,41 @@ def artifact_update_5(action=None, success=None, container=None, results=None, h
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="community/artifact_update", parameters=parameters, name="artifact_update_5")
+    phantom.custom_function(custom_function="community/artifact_update", parameters=parameters, name="artifact_update_5", callback=debug_8)
+
+    return
+
+
+@phantom.playbook_block()
+def debug_8(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("debug_8() called")
+
+    parameters = []
+
+    parameters.append({
+        "input_1": None,
+        "input_2": None,
+        "input_3": None,
+        "input_4": None,
+        "input_5": None,
+        "input_6": None,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_8")
 
     return
 
