4c4712efc73928a98d16cf6340bba035901e0232
Andreas <Schmoll>
2023-12-14 15:01:41 +0000

    a

--- APG_phish-cleanup.py
+++ APG_phish-cleanup.py
@@ -102,7 +102,7 @@ def artifact_update_1(action=None, success=None, container=None, results=None, h
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="community/artifact_update", parameters=parameters, name="artifact_update_1")
+    phantom.custom_function(custom_function="community/artifact_update", parameters=parameters, name="artifact_update_1", callback=join_add_tag_3)
 
     return
 
@@ -140,7 +140,7 @@ def artifact_update_2(action=None, success=None, container=None, results=None, h
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="community/artifact_update", parameters=parameters, name="artifact_update_2")
+    phantom.custom_function(custom_function="community/artifact_update", parameters=parameters, name="artifact_update_2", callback=join_add_tag_3)
 
     return
 
@@ -178,7 +178,45 @@ def artifact_update_5(action=None, success=None, container=None, results=None, h
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="community/artifact_update", parameters=parameters, name="artifact_update_5")
+    phantom.custom_function(custom_function="community/artifact_update", parameters=parameters, name="artifact_update_5", callback=join_add_tag_3)
+
+    return
+
+
+@phantom.playbook_block()
+def join_add_tag_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("join_add_tag_3() called")
+
+    # if the joined function has already been called, do nothing
+    if phantom.get_run_data(key="join_add_tag_3_called"):
+        return
+
+    # save the state that the joined function has now been called
+    phantom.save_run_data(key="join_add_tag_3_called", value="add_tag_3")
+
+    # call connected block "add_tag_3"
+    add_tag_3(container=container, handle=handle)
+
+    return
+
+
+@phantom.playbook_block()
+def add_tag_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("add_tag_3() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.add_tags(container=container, tags="transport_mail")
+
+    container = phantom.get_container(container.get('id', None))
 
     return
 
