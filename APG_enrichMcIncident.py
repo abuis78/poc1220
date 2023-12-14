52f42374b1d6c26bed84f862ccd6df162d8a615c
Andreas <Schmoll>
2023-12-14 14:35:04 +0000

    a

--- APG_enrichMcIncident.py
+++ APG_enrichMcIncident.py
@@ -65,21 +65,21 @@ def set_summary_fields_1(action=None, success=None, container=None, results=None
 
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
-    filtered_artifact_0_data_select_mc_incident = phantom.collect2(container=container, datapath=["filtered-data:select_mc_incident:condition_1:artifact:*.cef.id","filtered-data:select_mc_incident:condition_1:artifact:*.id","filtered-data:select_mc_incident:condition_1:artifact:*.external_id"])
-    filtered_artifact_1_data_select_reported_email = phantom.collect2(container=container, datapath=["filtered-data:select_reported_email:condition_1:artifact:*.cef.bodyHtml","filtered-data:select_reported_email:condition_1:artifact:*.id","filtered-data:select_reported_email:condition_1:artifact:*.external_id"])
+    filtered_artifact_0_data_select_reported_email = phantom.collect2(container=container, datapath=["filtered-data:select_reported_email:condition_1:artifact:*.cef.bodyHtml","filtered-data:select_reported_email:condition_1:artifact:*.id","filtered-data:select_reported_email:condition_1:artifact:*.external_id"])
+    filtered_artifact_1_data_select_mc_incident = phantom.collect2(container=container, datapath=["filtered-data:select_mc_incident:condition_1:artifact:*.cef.id","filtered-data:select_mc_incident:condition_1:artifact:*.id","filtered-data:select_mc_incident:condition_1:artifact:*.external_id"])
 
     parameters = []
 
     # build parameters list for 'set_summary_fields_1' call
-    for filtered_artifact_0_item_select_mc_incident in filtered_artifact_0_data_select_mc_incident:
-        for filtered_artifact_1_item_select_reported_email in filtered_artifact_1_data_select_reported_email:
-            if filtered_artifact_0_item_select_mc_incident[0] is not None:
+    for filtered_artifact_0_item_select_reported_email in filtered_artifact_0_data_select_reported_email:
+        for filtered_artifact_1_item_select_mc_incident in filtered_artifact_1_data_select_mc_incident:
+            if filtered_artifact_1_item_select_mc_incident[0] is not None:
                 parameters.append({
-                    "incident_id": filtered_artifact_0_item_select_mc_incident[0],
                     "pairs": [
-                        { "name": "body_html", "value": filtered_artifact_1_item_select_reported_email[0] },
+                        { "name": "body_html", "value": filtered_artifact_0_item_select_reported_email[0] },
                     ],
-                    "context": {'artifact_id': filtered_artifact_1_item_select_reported_email[1], 'artifact_external_id': filtered_artifact_1_item_select_reported_email[2]},
+                    "incident_id": filtered_artifact_1_item_select_mc_incident[0],
+                    "context": {'artifact_id': filtered_artifact_1_item_select_mc_incident[1], 'artifact_external_id': filtered_artifact_1_item_select_mc_incident[2]},
                 })
 
     ################################################################################
@@ -110,6 +110,7 @@ def code_1(action=None, success=None, container=None, results=None, handle=None,
     ################################################################################
 
     # Write your custom code here...
+    phantom.debug(filtered_artifact_0__cef_bodyhtml)
     output_file_path = "/tmp/output.pdf"
     # html_string_to_pdf returns False on error, and True on success    
     if not phantom.html_string_to_pdf(filtered_artifact_0__cef_bodyhtml, output_file_path):
