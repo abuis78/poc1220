a11301586c712d9d993e5a9d0309315a6dd50fc1
Andreas <Schmoll>
2023-12-19 13:17:58 +0000

    a

--- create_mc_incident.py
+++ create_mc_incident.py
@@ -110,7 +110,7 @@ def artifact_create_1(action=None, success=None, container=None, results=None, h
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="community/artifact_create", parameters=parameters, name="artifact_create_1", callback=join_select_mc_id)
+    phantom.custom_function(custom_function="community/artifact_create", parameters=parameters, name="artifact_create_1", callback=select_mc_id)
 
     return
 
@@ -129,56 +129,10 @@ def does_mc_incident_already_exist(action=None, success=None, container=None, re
 
     # call connected blocks if condition 1 matched
     if found_match_1:
-        join_select_mc_id(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'else' condition 2
     format_icident_name(action=action, success=success, container=container, results=results, handle=handle)
-
-    return
-
-
-@phantom.playbook_block()
-def debug_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("debug_2() called")
-
-    parameters = []
-
-    parameters.append({
-        "input_1": None,
-        "input_2": None,
-        "input_3": None,
-        "input_4": None,
-        "input_5": None,
-        "input_6": None,
-        "input_7": None,
-        "input_8": None,
-        "input_9": None,
-        "input_10": None,
-    })
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_2")
-
-    return
-
-
-@phantom.playbook_block()
-def join_select_mc_id(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("join_select_mc_id() called")
-
-    if phantom.completed(custom_function_names=["artifact_create_1"]):
-        # call connected block "select_mc_id"
-        select_mc_id(container=container, handle=handle)
 
     return
 
@@ -191,14 +145,14 @@ def select_mc_id(action=None, success=None, container=None, results=None, handle
     matched_artifacts_1, matched_results_1 = phantom.condition(
         container=container,
         conditions=[
-            ["mc_id", "in", "artifact..*.name"]
+            ["mc_id", "in", "artifact.*.name"]
         ],
         name="select_mc_id:condition_1",
         delimiter=None)
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        debug_2(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+        pass
 
     return
 
