20cfca32ae61313fda4ade3d016089aaf82ebe4b
Andreas <Schmoll>
2023-12-19 13:14:13 +0000

    a

--- create_mc_incident.py
+++ create_mc_incident.py
@@ -110,7 +110,7 @@ def artifact_create_1(action=None, success=None, container=None, results=None, h
     ## Custom Code End
     ################################################################################
 
-    phantom.custom_function(custom_function="community/artifact_create", parameters=parameters, name="artifact_create_1")
+    phantom.custom_function(custom_function="community/artifact_create", parameters=parameters, name="artifact_create_1", callback=join_select_mc_id)
 
     return
 
@@ -129,20 +129,33 @@ def does_mc_incident_already_exist(action=None, success=None, container=None, re
 
     # call connected blocks if condition 1 matched
     if found_match_1:
-        format_icident_name(action=action, success=success, container=container, results=results, handle=handle)
-        action_1(action=action, success=success, container=container, results=results, handle=handle)
+        join_select_mc_id(action=action, success=success, container=container, results=results, handle=handle)
         return
+
+    # check for 'else' condition 2
+    format_icident_name(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
 
 @phantom.playbook_block()
-def action_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("action_1() called")
-
-    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+def debug_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("debug_2() called")
 
     parameters = []
+
+    parameters.append({
+        "input_1": None,
+        "input_2": None,
+        "input_3": None,
+        "input_4": None,
+        "input_5": None,
+        "input_6": None,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
 
     ################################################################################
     ## Custom Code Start
@@ -153,6 +166,39 @@ def action_1(action=None, success=None, container=None, results=None, handle=Non
     ################################################################################
     ## Custom Code End
     ################################################################################
+
+    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_2")
+
+    return
+
+
+@phantom.playbook_block()
+def join_select_mc_id(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("join_select_mc_id() called")
+
+    if phantom.completed(custom_function_names=["artifact_create_1"]):
+        # call connected block "select_mc_id"
+        select_mc_id(container=container, handle=handle)
+
+    return
+
+
+@phantom.playbook_block()
+def select_mc_id(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("select_mc_id() called")
+
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["mc_id", "in", "artifact..*.name"]
+        ],
+        name="select_mc_id:condition_1",
+        delimiter=None)
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        debug_2(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     return
 
@@ -161,6 +207,14 @@ def action_1(action=None, success=None, container=None, results=None, handle=Non
 def on_finish(container, summary):
     phantom.debug("on_finish() called")
 
+    create_incidents_1_result_data = phantom.collect2(container=container, datapath=["create_incidents_1:action_result.data.*.id"])
+
+    create_incidents_1_result_item_0 = [item[0] for item in create_incidents_1_result_data]
+
+    output = {
+        "mc_id": create_incidents_1_result_item_0,
+    }
+
     ################################################################################
     ## Custom Code Start
     ################################################################################
@@ -170,5 +224,7 @@ def on_finish(container, summary):
     ################################################################################
     ## Custom Code End
     ################################################################################
+
+    phantom.save_playbook_output_data(output=output)
 
     return
