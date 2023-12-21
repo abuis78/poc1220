8999e6b2a557267b2646bd342ce1241769f40417
Andreas <Schmoll>
2023-12-21 10:10:19 +0000

    a

--- APG_create-mc-incident.py
+++ APG_create-mc-incident.py
@@ -115,10 +115,14 @@ def mc_incident_alread_exists(action=None, success=None, container=None, results
 def debug_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("debug_2() called")
 
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.name","artifact:*.id","artifact:*.external_id"])
+
+    container_artifact_header_item_0 = [item[0] for item in container_artifact_data]
+
     parameters = []
 
     parameters.append({
-        "input_1": ["artifact.*.name"],
+        "input_1": container_artifact_header_item_0,
         "input_2": None,
         "input_3": None,
         "input_4": None,
