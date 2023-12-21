92e5aab87259e751aba940c422ee92b51a322d9e
Andreas <Schmoll>
2023-12-21 10:04:53 +0000

    a

--- APG_extract-reportedMail.py
+++ APG_extract-reportedMail.py
@@ -243,7 +243,45 @@ def filter_email_artifact_types(action=None, success=None, container=None, resul
 
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
-        pass
+        debug_6(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+
+@phantom.playbook_block()
+def debug_6(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("debug_6() called")
+
+    filtered_artifact_0_data_filter_email_artifact_types = phantom.collect2(container=container, datapath=["filtered-data:filter_email_artifact_types:condition_1:artifact:*.id","filtered-data:filter_email_artifact_types:condition_1:artifact:*.id","filtered-data:filter_email_artifact_types:condition_1:artifact:*.external_id"])
+
+    filtered_artifact_0__id = [item[0] for item in filtered_artifact_0_data_filter_email_artifact_types]
+
+    parameters = []
+
+    parameters.append({
+        "input_1": filtered_artifact_0__id,
+        "input_2": None,
+        "input_3": None,
+        "input_4": None,
+        "input_5": None,
+        "input_6": None,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_6")
 
     return
 
